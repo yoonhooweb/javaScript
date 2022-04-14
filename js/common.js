8a0d104c7f44af36650695e918ea8d351901ee4a
joyoonhoo <showil20@naver.com>
2022-04-15 00:02:54 +0900

    left 메뉴 및 사진 30x30으로 수정해야함

--- js/common.js
+++ js/common.js
@@ -11,6 +11,18 @@ $(document).ready(function(){
             }
         }
     });
+
+    $(".sideBarBt").click(function(){
+        if ( $(this).find("i").hasClass("xi-hamburger-back") ) {
+            $(this).find("i").removeClass("xi-hamburger-back");
+            $(this).find("i").addClass("xi-hamburger-out");
+            $(".sideBar").css("width", "50px").addClass("active");
+        } else if ($(this).find("i").hasClass("xi-hamburger-out")) {
+            $(this).find("i").addClass("xi-hamburger-back");
+            $(this).find("i").removeClass("xi-hamburger-out");
+            $(".sideBar").css("width", "200px").removeClass("active");
+        }
+    })
 })
 
 function arrowRotate(){
@@ -18,8 +30,10 @@ function arrowRotate(){
     let userInfo = document.querySelector(".userInfo");
     rotate.classList.toggle("active");
     if (rotate.classList.contains("active")) {
+        userInfo.style.height = "200px";
         userInfo.style.display = "block";
     } else {
+        userInfo.style.height = "0px";
         userInfo.style.display = "none";
     }
 }
