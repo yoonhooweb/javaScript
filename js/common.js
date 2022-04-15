2e7ccc4eb7614362193911b12d2d6a177c850c6e
joyoonhoo <showil20@naver.com>
2022-04-15 18:27:02 +0900

    테이블 진행중

--- js/common.js
+++ js/common.js
@@ -16,14 +16,26 @@ $(document).ready(function(){
         if ( $(this).find("i").hasClass("xi-hamburger-back") ) {
             $(this).find("i").removeClass("xi-hamburger-back");
             $(this).find("i").addClass("xi-hamburger-out");
+            $(".contentArea").css("width","calc(100% - 90px)");
             $(".sideBar").css("width", "50px").addClass("active");
         } else if ($(this).find("i").hasClass("xi-hamburger-out")) {
             $(this).find("i").addClass("xi-hamburger-back");
             $(this).find("i").removeClass("xi-hamburger-out");
             $(".sideBar").css("width", "200px").removeClass("active");
+            $(".contentArea").css("width","calc(100% - 240px)");
             console.log("git Test");
         }
     })
+
+    $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+		$('ul.tabs li').removeClass('active');
+		$('.tab-contents').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+        console.log(tab_id);
+	})
 })
 
 function arrowRotate(){
