14e820849b8e9cf3c81b16fd3303c93e31a49fd8
joyoonhoo <showil20@naver.com>
2022-06-24 18:04:24 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -1,80 +1,40 @@
-$(document).ready(function(){
-    let menuCheckli = $(".gnb .depth1");
-    let menuCheck = $(".gnb")
-    $(menuCheckli).bind({"mouseover" : function(){
-        if (!menuCheck.hasClass("active")) {
-            $(menuCheck).addClass("active");
-        }
-        }, "mouseleave" :  function (){
-            if (menuCheck.hasClass("active")) {
-                $(menuCheck).removeClass("active");
-            }
-        }
-    });
-
-    $('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-		$('ul.tabs li').removeClass('active');
-		$('.tab-contents').removeClass('active');
-
-		$(this).addClass('active');
-		$("#"+tab_id).addClass('active');
-	})
-
-    $("#stDate,#endDate").datepicker({
-        dateFormat: 'yy-mm-dd'
-        ,showOtherMonths: true
-        ,showMonthAfterYear:true
-        ,dayNamesMin: ['일','월','화','수','목','금','토']
-        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
-    });
-
-    $(".addRow").click(function(){
-        let html = '';
-        let tbLength = $(".table-detail tr").length;
-        console.log(tbLength);
-        html += '<tr>';
-        html += '<th>추가</th>';
-        html += '<td>추우우우가</td>';
-        html += '</tr>';
-        $(".table-detail tr:last-child").after(html);
-    })
-
-});
-
-/* function arrowRotate(){
-    let rotate = document.querySelector(".userArrow");
-    let userInfo = document.querySelector(".userInfo");
-    rotate.classList.toggle("active");
-    if (rotate.classList.contains("active")) {
-        userInfo.style.height = "200px";
-        userInfo.style.display = "block";
-    } else {
-        userInfo.style.height = "0px";
-        userInfo.style.display = "none";
-    }
-} */
-
-(function () {
-    let modalopen = document.querySelector(".modal");
-    let modalWrap = document.querySelector("#modal");
-    modalopen.addEventListener("click", function(){
-        modalWrap.style.display = "block";
-    });
-
-    let modal = document.querySelector("#modal");
-    let close = document.querySelector(".modal-close");
-    close.addEventListener("click", function(){
-        modal.style.display = "none";
-    });
-
-    let sideBt = document.querySelector(".side li a");
-    sideBt.addEventListener("click", function(e){
-        /* if (sideBt.classList.contains("active")) {
-            sideBt.classList.remove("active");
-        } else {
-            sideBt.classList.add("active");
-        } */
-        sideBt.classList.toggle("active");
-    })
+(function(){
+    userInfo();
+    searchBox();
+    leftBt();
 })();
+
+function userInfo() {
+    let userIcon = document.querySelector(".userIcon");
+    let userBox = document.querySelector(".userInfo-wrap");
+    let arrow = document.querySelector(".userArrow");
+    userIcon.addEventListener("click",function(){
+        console.log("test");
+        if (userBox.classList.contains('active')) {
+            userBox.classList.remove('active');
+            arrow.classList.add('xi-caret-down-min');
+            arrow.classList.remove('xi-caret-up-min');
+        } else { 
+            userBox.classList.add('active');
+            arrow.classList.remove('xi-caret-down-min');
+            arrow.classList.add('xi-caret-up-min');
+        }
+    });
+};
+
+function searchBox() {
+    let search = document.querySelector(".search");
+    let searchBox = document.querySelector(".search-box");
+    let userBox = document.querySelector(".userInfo-wrap");
+    search.addEventListener("click",function(){
+        searchBox.classList.toggle("active");
+    });
+};
+
+function leftBt() {
+    let sideBt = document.querySelector(".left-navBt");
+    let side = document.querySelector(".side-nav");
+    sideBt.addEventListener("click",function(){
+        side.classList.toggle("active");
+    })
+}
