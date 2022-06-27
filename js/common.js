a4ea7e93d36a25d169d593acb47d01b905e79448
joyoonhoo <showil20@naver.com>
2022-06-27 18:14:07 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -1,10 +1,24 @@
-(function(){
-    userInfo();
-    searchBox();
-    leftBt();
-})();
+$(document).ready(function(){
 
-function userInfo() {
+    $("#stDate,#endDate").datepicker({
+        dateFormat: 'yy-mm-dd'
+        ,showOtherMonths: true
+        ,showMonthAfterYear:true
+        ,dayNamesMin: ['일','월','화','수','목','금','토']
+        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
+    });
+
+    $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+		$('ul.tabs li').removeClass('active');
+		$('.tab-contents').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+	});
+});
+
+(function userInfo() {
     let userIcon = document.querySelector(".userIcon");
     let userBox = document.querySelector(".userInfo-wrap");
     let arrow = document.querySelector(".userArrow");
@@ -20,21 +34,70 @@ function userInfo() {
             arrow.classList.add('xi-caret-up-min');
         }
     });
-};
+})();
 
-function searchBox() {
+(function searchBox() {
     let search = document.querySelector(".search");
     let searchBox = document.querySelector(".search-box");
     let userBox = document.querySelector(".userInfo-wrap");
     search.addEventListener("click",function(){
         searchBox.classList.toggle("active");
     });
-};
+})();
 
-function leftBt() {
+/* function leftBt() {
+    let sideBt = document.querySelector(".left-navBt");
+    let side = document.querySelector(".side-nav");
+    let active = sessionStorage.getItem("active");
+    sideBt.addEventListener("click",function(){
+        sessionStorage.removeItem(active);
+        if ( side.classList.contains(active) ) {
+            side.classList.remove(active);
+            sessionStorage.removeItem(active);
+        } else {
+            sessionStorage.setItem("active" , "active");
+            side.classList.add(active);
+
+        }
+
+    })
+} */
+
+(function leftBt() {
+    let main = document.querySelector("#main");
     let sideBt = document.querySelector(".left-navBt");
     let side = document.querySelector(".side-nav");
     sideBt.addEventListener("click",function(){
-        side.classList.toggle("active");
+        if ( side.classList.contains("active") ) {
+            side.classList.remove("active");
+            main.classList.remove("active");
+        } else {
+            side.classList.add("active");
+            main.classList.add("active");
+        }
+
     })
-}
+})();
+
+(function modalVersion1(){
+    let modalopen = document.querySelector(".modal-version1");
+    let modal = document.querySelector(".version-1");
+    modalopen.addEventListener("click", function(){
+        modal.style.display = "block";
+    });
+
+    let close = document.querySelector(".modal-close");
+    close.addEventListener("click", function(){
+        modal.style.display = "none";
+    });
+})();
+
+
+(function tableRow(){
+    let tableHeader = document.querySelector(".table-header");
+    let tr = document.querySelector(".table-body tbody").clientHeight;
+    console.log(tr > 300);
+    if (tr > 300) {
+        tableHeader.style.padding = '0px 20px 0px 0px';
+    }
+})();   
