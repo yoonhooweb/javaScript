8b9449b44aa88ab263670198b53b09df76f2861e
joyoonhoo <showil20@naver.com>
2024-04-17 18:07:38 +0900

    화면 커밋

--- hackerNews/js/app.js
+++ hackerNews/js/app.js
@@ -1,36 +1,69 @@
 const ajax = new XMLHttpRequest();  // ajax 사용하기위한 선언
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 let CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const div_root = document.getElementById('root');
-const ul = document.getElementById('ul');
-const content_box = document.getElementById('content');
+const container = document.getElementById('root');
+const ul = document.createElement('ul');
+const content_box = document.createElement('div');
+const store = {
+    currentPage : 1,
+}
 
-ajax.open('GET', NEWS_URL, false); // 데이터 통신을 위한 시작
-ajax.send();  //데이터 통신 보내기
-
-let newsFeed = JSON.parse(ajax.response); //ajax.response는 데이터 받고 json.parse로 json데이터 변환
-
-for (let i = 0; i < newsFeed.length; i++) {
-    const li = document.createElement('li'); // li 태그 생성하는것
-    const a = document.createElement('a');  // a태그 생성하는것
-
-    a.href = `#${newsFeed[i].id}`;  //a 태그 생성한 후 URL 넣어주기
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;  // a태그 안에 데이터 넣기
-    a.dataset.title = newsFeed[i].id;   //data-title 만들기
-
-    a.addEventListener("click", function(){});
-
-    li.appendChild(a);
-    ul.appendChild(li);
-};
-
-window.addEventListener('hashchange', function(){
-    const ID = location.hash.substring(1);  //첫번째 것을 제외한 값으로 변경 
-    ajax.open("GET", CONTENT_URL.replace('@id', ID), false); //replace : '기존값' , '바뀌는값'
+function getData (url) {
+    ajax.open("GET", url, false);
     ajax.send();
-    let news_contents = JSON.parse(ajax.response);
+
+    return JSON.parse(ajax.response);
+} 
+
+/* ajax.open('GET', NEWS_URL, false); // 데이터 통신을 위한 시작
+ajax.send();  //데이터 통신 보내기 */
+
+let newsFeed = getData(NEWS_URL);; //ajax.response는 데이터 받고 json.parse로 json데이터 변환
+container.appendChild(ul);
+
+function feed() {
+    for (let i = 0; i < newsFeed.length; i++) {
+        const li = document.createElement('li'); // li 태그 생성하는것
+        const div = document.createElement('div');
+    
+        div.innerHTML = `
+            <li>
+                <a href='#${newsFeed[i].id}' data-url=${newsFeed[i].url}>${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+            </li>
+        `
+        //appendChild div를 하게되면 div자체가 들어가버리지만 firstElementChild 를 하게 되면 li가 자식요소로써 들어가게된다.
+        ul.appendChild(div.firstElementChild);
+        div.innerHTML = `
+            <ul>
+                <li><a href="#${store.currentPage - 1}">이전 페이지</a></li>
+                <li><a href="#${store.currentPage + 1}">다음 페이지</a></li>
+            </ul>
+        `
+    };    
+}
+
+function newsDetail(){
+    const ID = location.hash.substring(1);  //첫번째 것을 제외한 값으로 변경 
+    let news_contents = getData(CONTENT_URL.replace("@id", ID));
     const title = document.createElement('h1');
 
-    title.innerHTML = news_contents.title;
-    content_box.appendChild(title);
-});
+    container.innerHTML = `
+        <h1>${news_contents.title}</h1>
+        <a href=''>목록으로</a>
+    `
+}
+
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') {
+        feed();
+    } else {
+        newsDetail();
+    }
+    
+}
+
+window.addEventListener('hashchange', router);
+
+router();
