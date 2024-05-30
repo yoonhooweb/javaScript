1e756189d6e817e1b13fd8f4b31a1a7ce1a790b3
조윤후 <showil20@sayinfo.co.kr>
2024-05-30 13:55:09 +0900

    커밋

---
+++ js/grammer/main.js
@@ -0,0 +1,13 @@
+function sum( ) {
+    return console.log(arguments);
+} 
+
+sum(1,5);
+
+
+let x  = 6;
+let functionName = (v) => {
+     return v * 2
+}
+
+console.log(`functionName : ${functionName(x)}`);
